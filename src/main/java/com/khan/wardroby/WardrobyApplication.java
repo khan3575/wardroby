1d66ca809ae2227d7eb440070e0001d3c77f82d3
sakibkhan <sakibkhan.stud@gmail.com>
2026-06-01 15:21:40 +0600

    first commit

---
+++ src/main/java/com/khan/wardroby/WardrobyApplication.java
@@ -0,0 +1,13 @@
+package com.khan.wardroby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WardrobyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WardrobyApplication.class, args);
+	}
+
+}
